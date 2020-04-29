60ffda7d7e2aa5594d9dfb83848a947335d8b41e
savicmilan <63342796+savicmilan@users.noreply.github.com>
2020-04-29 11:40:58 +0200

    Add files via upload

--- oruzije.hpp
+++ oruzije.hpp
@@ -1,6 +1,8 @@
 #ifndef ORUZIJE_HPP_INCLUDED
 #define ORUZIJE_HPP_INCLUDED
 
+
+#define MAXX=40;
 enum vrsta {mac,luk,sekira,koplje,magija};
 
 class Oruzije
@@ -13,7 +15,7 @@ public:
     Oruzije()
     {
         oruzijee=0;
-        durability=40;
+        durability=MAXX;
         jacina=6;
     }
     Oruzije(vrsta or, int dd, int jj)
@@ -28,6 +30,29 @@ public:
         durability=o.durability;
         jacina=o.jacina;
     }
+
+    void smanjiDur()
+    {
+        if(durability!=0)
+            durability--;
+    }
+    //kada se iskoristi oruzije ovaj broj se smanjuje jer duravility predstavlja broj preostalih koriscenja
+    void popraviOruzije()
+    {
+        if(durability!=MAXX)
+            durability=40;
+    }
+    //ovo popravlja oruzije ako je korisceno i ako zatreba
+    void pojacajOruzije()
+    {
+        jacina++;
+    }
+    //ovo ostri oruzije da bi ono bilo jace
+    void pojacajOruzijeZA(int x)
+    {
+        jacina=jacina+x;
+    }
+    //ovo ostri oruzije ali malo bolje nego ono ranije, ovo bi trebalo da se otkljuca kasnije u igrici
 };
 
 #endif // ORUZIJE_HPP_INCLUDED

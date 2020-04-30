2dcc4fc38c8ef5009b52b4e7c4b78f08d8a1718e
savicmilan <63342796+savicmilan@users.noreply.github.com>
2020-04-30 16:29:28 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+// ne znam da objasnim bas sta ce mi kod skroz raditi pa cu da stavim link igrice koju pravim, bice losije naravno https://www.youtube.com/watch?v=NbG2b-ckJBs
+
 int main()
 {
     cout << "Hello world!" << endl;

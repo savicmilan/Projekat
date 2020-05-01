45011b93e334e5e90a999f9dc85f1ecc788fe777
savicmilan <63342796+savicmilan@users.noreply.github.com>
2020-05-01 18:40:18 +0200

    Add files via upload

--- bojnopolje.hpp
+++ bojnopolje.hpp
@@ -2,12 +2,21 @@
 #define BOJNOPOLJE_HPP_INCLUDED
 
 #include "karakter.hpp"
+typedef struct PozicijaKaraktera
+{
+    Karakter kaka;
+    int x;
+    int y;
+} POZICIJAKARAKTERA;
 
 class Bojnopolje
 {
 private:
     int polje[10][10];
-    double pozicijeKaraktera[20][1];// Prvi broj je id karaktera, a drugi ce biti njihova pozicija
+    POZICIJAKARAKTERA karekteri;
+
+public:
+    //void PomeriSe()
 
 };
 
